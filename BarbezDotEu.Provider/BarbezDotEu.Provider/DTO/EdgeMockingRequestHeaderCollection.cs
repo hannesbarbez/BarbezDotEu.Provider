6adc8a358c9f323974d263ae5abfc5535b8bd657
Hannes Barbez <hannesbarbez@hotmail.com>
2022-12-03 18:30:02 +0100

    Bump

--- BarbezDotEu.Provider/BarbezDotEu.Provider/DTO/EdgeMockingRequestHeaderCollection.cs
+++ BarbezDotEu.Provider/BarbezDotEu.Provider/DTO/EdgeMockingRequestHeaderCollection.cs
@@ -23,7 +23,7 @@ namespace BarbezDotEu.Provider.DTO
         /// <summary>
         /// Gets an Edge style user agent header.
         /// </summary>
-        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33";
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62";
 
         /// <summary>
         /// Gets Edge style accept headers.
