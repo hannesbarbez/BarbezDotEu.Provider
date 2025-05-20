45a7affe3612830933b2f08c3645a9539ce2f907
Hannes Barbez <hannesbarbez@hotmail.com>
2025-05-20 20:39:46 +0200

    Bumps

---
+++ BarbezDotEu.Provider/BarbezDotEu.Provider.Tests/PoliteProviderCan.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BarbezDotEu.Provider.Tests
+{
+    public class PoliteProviderCan
+    {
+        private readonly IHost _host;
+
+        public PoliteProviderCan()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddLogging();
+                    services.AddHttpClient();
+                }).Build();
+        }
+
+        [Fact]
+        public async Task GetInsiderFinancialRssXmlAsync()
+        {
+            // Arrange
+            var httpClientFactory = _host.Services.GetService<IHttpClientFactory>();
+            var logger = _host.Services.GetService<ILogger<TestablePoliteProvider>>();
+            Assert.NotNull(httpClientFactory);
+            Assert.NotNull(logger);
+
+            // Act
+            var provider = new TestablePoliteProvider(logger, httpClientFactory);
+            (string html, string uri) = await provider.GetInsiderFinancialRssXml();
+
+            // Assert
+            Assert.NotNull(html);
+            Assert.NotNull(uri);
+        }
+    }
+}
---
+++ BarbezDotEu.Provider/BarbezDotEu.Provider.Tests/TestablePoliteProvider.cs
@@ -0,0 +1,30 @@
+﻿using BarbezDotEu.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BarbezDotEu.Provider.Tests
+{
+    public class TestablePoliteProvider(ILogger logger, IHttpClientFactory httpClientFactory) : PoliteProvider(logger, httpClientFactory)
+    {
+        public async Task<(string html, string uri)> GetInsiderFinancialRssXml()
+        {
+            string uri = "https://www.insiderfinancial.com/post/rss.xml";
+            return (await Get(uri), uri);
+        }
+
+        private async Task<string> Get(string url)
+        {
+            var uri = new Uri(url);
+            var headers = new EdgeMockingRequestHeaderCollection($"https://{uri.Host}/");
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request = headers.Prep(request);
+            var result = await this.Request<string>(request);
+            if (result != null && result.HasFailed)
+            {
+                this.Logger.LogError("Request to {uri} failed: {error}", url, result.HttpResponseMessage);
+                return null;
+            }
+
+            return result.Content;
+        }
+    }
+}
